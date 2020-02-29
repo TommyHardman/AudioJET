7d635d4effba050043f37601657e6c29216ccb44
TommyHardman <tommydhardman@gmail.com>
2020-02-29 09:48:44 +0000

    improvements

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,57 +1,110 @@
 #include "ofApp.h"
 
-#define FS 44100
+//------------------------------------------------------------
+float powFreq(float i) {
+    return powf(i, 3);
+}
 
 //------------------------------------------------------------
 void ofApp::setup() {
 	ofSetVerticalSync(true);
 
-	plotHeight = 128;
-	bufferSize = 2048;
-    
     ofSoundStreamListDevices();
-
-	fft = ofxFft::create(bufferSize, OF_FFT_WINDOW_HAMMING);
-	// To use FFTW, try:
-	//fft = ofxFft::create(bufferSize, OF_FFT_WINDOW_HAMMING, OF_FFT_FFTW);
-
-
-	drawBuffer.resize(bufferSize);
-	middleBuffer.resize(bufferSize);
-	audioBuffer.resize(bufferSize);
-	
-	drawBins.resize(fft->getBinSize());
-	middleBins.resize(fft->getBinSize());
-	audioBins.resize(fft->getBinSize());
+    
+    plotHeight = 128;
+    fft.setup(16384);
     
     midiNotes.resize(0);
 
-	// 0 output channels,
-	// 1 input channel
-	// 44100 samples per second
-	// [bins] samples per buffer
-	// 4 num buffers (latency)
-    
-    soundStream.setDeviceID(0);
-    
-	//ofSoundStreamSetup(0, 1, this, 44100, bufferSize, 4);
-    soundStream.setup(this, 0, 1, FS, bufferSize, 4);
-    
-	mode = MIC;
-	appWidth = ofGetWidth();
-	appHeight = ofGetHeight();
-
-	ofBackground(0, 0, 0);
-    
     midiFile.clear(); // clear file and save
     midiFile.save("midi.mid");
     
     // Gui
     gui.setup();
-    gui.add(time_threshold.setup("time threshold",-20, plotHeight/2, -plotHeight/2));
+    gui.add(time_threshold.setup("time threshold",64, -plotHeight/2, plotHeight/2));
     gui.add(freq_threshold.setup("freq threshold",100,0, plotHeight));
+    gui.add(generate.setup("Generate Midi",200,30));
+    generate.addListener(this, &ofApp::exportMidi);
+//    gui.setPosition(543, 365);
+    
+    
+    // Title text
+    verdana30.load("verdana.ttf", 30, true, true);
+    verdana30.setLineHeight(34.0f);
+    verdana30.setLetterSpacing(1.035);
     
 }
+
+//------------------------------------------------------------
+void ofApp::update() {
+    fft.update();
+    
+    // calculate peaks
+    freqPeaks(fft.getBins(), plotHeight, freq_threshold);
+    timePeaks(fft.getAudio(), plotHeight/2, time_threshold);
+}
+
+//------------------------------------------------------------
+void ofApp::plot(vector<float>& buffer, float scale, float threshold) {
+    ofNoFill();
+    int n = MIN(1024, buffer.size());
+
+    ofPushMatrix();
+    ofTranslate(ofGetWidth()/2 - n/2, scale);
+    ofSetColor(255, 0, 0);
+    ofDrawLine(0, -threshold,n , -threshold); // threshold line
+
+    ofBeginShape();
+    for (int i = 0; i < n; i++) {
+        ofSetColor(255, 255, 255); // white signal
+        ofVertex(i, buffer[i] * -scale);
+        if (buffer[i] * scale > threshold) {
+            ofSetColor(255, 0, 0); // red thresholding
+            ofDrawCircle(i,buffer[i] * -scale,10);
+        }
+    }
+    ofEndShape();
+    ofPopMatrix();
+}
+
+//------------------------------------------------------------
+void ofApp::timePeaks(vector<float>& buffer, float scale, float threshold) { // updates time interval vector
+    int n = MIN(1024, buffer.size());
+    
+    int peaks = 0;
+    timeMarkers.clear();
+    for (int i = 0; i < n; i++) {
+        if (buffer[i] * scale > threshold) {
+            peaks++;
+            float t = (i * (1.0/fft.stream.getSampleRate())) * 1000; // time (ms)
+            
+            timeMarkers.resize(peaks);
+            timeMarkers.push_back(t);
+        }
+    }
+}
+
+//------------------------------------------------------------
+void ofApp::freqPeaks(vector<float>& buffer, float scale, float threshold) { // updates frequency peak vector and calculates midiNotes
+    int n = MIN(1024, buffer.size());
+    
+    int peaks = 0;
+    midiNotes.clear();
+    for (int i = 0; i < n; i++) {
+        if (buffer[i] * scale > threshold) {
+            peaks++;
+        
+            float f = ((fft.stream.getSampleRate() * 0.5) / buffer.size()) * i;                      // frequency (Hz)
+            int midiNote = 12 * log2(f / 440.0) + 69;    // midi
+            
+            midiNotes.resize(peaks);
+            midiNotes.push_back(midiNote);
+            
+        }
+    }
+   
+}
+
 
 //------------------------------------------------------------
 void ofApp::shuffle_midiNotes() {
@@ -75,12 +128,13 @@ int ofApp::roundUp(int numToRound, int multiple) {
     return numToRound + multiple - remainder;
 }
 
+
 //------------------------------------------------------------
 void ofApp::exportMidi() {
     shuffle_midiNotes();
     shuffle_timeMarkers();
     
-    float chunkTime = (bufferSize * 1000) / FS; // milliseconds
+    float chunkTime = ((fft.getAudioBufferSize() * 4 * 1000)) / fft.stream.getSampleRate(); // max signal length in milliseconds
     
     int interval_map = 0;
     for (int i = 1; i < midiNotes.size(); i++) { // first element is 0
@@ -89,7 +143,6 @@ void ofApp::exportMidi() {
         int tmp_val = (int)ofMap(interval,0,chunkTime,47,384); // limits ?
         tmp_val = roundUp(tmp_val, 96);
         
-     
         midiFile.addNoteOn(midiNotes[i], 127, interval_map);
         midiFile.addNoteOff(midiNotes[i], 127, interval_map + tmp_val);
         interval_map += tmp_val;
@@ -102,218 +155,68 @@ void ofApp::exportMidi() {
 
 //------------------------------------------------------------
 void ofApp::draw() {
+    ofBackground(50);
 	ofSetColor(255);
+    
 	ofPushMatrix();
 	ofTranslate(16, 16);
-	ofDrawBitmapString("Time Domain", 0, 0);
-	
-	soundMutex.lock();
-	drawBuffer = middleBuffer;
-	drawBins = middleBins;
-	soundMutex.unlock();
-	
-	plot(drawBuffer, plotHeight / 2, 0, false);
-    timePeaks(drawBuffer, plotHeight / 2, 0, time_threshold);
-    
+    plot(fft.getAudio(), plotHeight/2, time_threshold); // draw time signal
     
 	ofTranslate(0, plotHeight + 16);
-	ofDrawBitmapString("Frequency Domain", 0, 0);
-	plot(drawBins, -plotHeight, plotHeight / 2, true);
+    plot(fft.getBins(), plotHeight, freq_threshold); // draw frequency signal
     ofPopMatrix();
     
-    ofPushMatrix();
-    ofTranslate(16,plotHeight + 16 + 16);
-    // draw circle at highest peaks
-    freqPeaks(drawBins, -plotHeight, plotHeight / 2, freq_threshold);
-    
-    ofPopMatrix();
     
     if (PAUSE) {
         ofSetColor(255);
+        ofDrawBitmapString("Midi Notes :", 700, 200);
         for (int i = 1; i < midiNotes.size(); i++) { // first index is 0
-            ofDrawBitmapString(ofToString(midiNotes[i]),200 + i*50 , ofGetHeight() - 200);
+            ofDrawBitmapString(ofToString(midiNotes[i]),700, 200 + (i * 15));
             // export to midi file
         }
-        for (int i = 1; i < timeMarkers.size(); i++) { 
-            ofDrawBitmapString(ofToString(timeMarkers[i]),200 + i*50 , ofGetHeight() - 300);
+        ofDrawBitmapString("Time markers :", 900, 200);
+        for (int i = 1; i < timeMarkers.size(); i++) {
+            if (i < 30) {
+                ofDrawBitmapString(ofToString(timeMarkers[i]),900, 200 + (i * 15));
+            }
             
         }
     }
     
+    // Titles & Gui
     ofPushMatrix();
-    //ofScale(10);
-    ofSetColor(255);
-    ofDrawBitmapString("Audio JET", ofGetWidth()/2, ofGetHeight()/2);
+    ofSetColor(255,20,50);
+    ofRectangle rect = verdana30.getStringBoundingBox(TITLE, ofGetWidth()/2, ofGetHeight()/2);
+    verdana30.drawString(TITLE, ofGetWidth()/2 - rect.width/2, ofGetHeight()/2 - rect.height/2);
     ofPopMatrix();
     
+    ofSetColor(200);
+    ofDrawBitmapString("...Press spacebar to pause audio stream...",ofGetWidth()/2, ofGetHeight() - 100);
+    
+    gui.setPosition(ofGetWidth()/2 - gui.getWidth()/2, ofGetHeight()/2 + gui.getHeight()/2);
     gui.draw();
 	
 }
 
-//------------------------------------------------------------
-float powFreq(float i) {
-	return powf(i, 3);
-}
+
 
 //------------------------------------------------------------
-void ofApp::timePeaks(vector<float> buffer, float scale, float offset, float threshold) {
-    int n = buffer.size();
-  
-    glPushMatrix();
-    glTranslatef(0, plotHeight/2 + offset, 0);
-    ofSetColor(255, 255, 0);
-    ofDrawLine(0, threshold, n*0.5, threshold); // threshold line
-    
-    int peaks = 0;
-    timeMarkers.clear();
-    for (int i = 0; i < n; i++) {
-        float val = (buffer[i] * scale);
-        if (val < threshold) {
-            peaks++;
-            //cout << val << endl;
-            
-            float t = (i * (1.0/FS)) * 1000; // time (ms)
-          
-            timeMarkers.resize(peaks);
-            timeMarkers.push_back(t);
-
-            ofDrawCircle(i*0.5, buffer[i] * scale, 5);
-            ofDrawBitmapString(ofToString(t),i*0.5, 600); // times
-            
-            
-        }
-    }
-    glPopMatrix();
-}
-
-//------------------------------------------------------------
-void ofApp::freqPeaks(vector<float> buffer, float scale, float offset, float threshold) {
-    int n = buffer.size();
-    int factor = 10;
-    glPushMatrix();
-    glTranslatef(0, plotHeight/2 + offset, 0);
-    ofSetColor(255, 0, 0);
-    ofDrawLine(0, -threshold,(n/16) * factor, -threshold); // threshold line
-    
-    int peaks = 0;
-    midiNotes.clear();
-    for (int i = 0; i < n/16; i++) {
-        float val = abs(buffer[i] * scale);
-        if (val > threshold) {
-            peaks++;
-            //cout << val << endl;
-            float f = (FS/2)/n * i;                      // frequency (Hz)
-            int midiNote = 12 * log2(f / 440.0) + 69;    // midi
-            
-            midiNotes.resize(peaks);
-            midiNotes.push_back(midiNote);
-            
-            ofDrawCircle(i*factor, buffer[i] * scale, 5);
-            ofDrawBitmapString(ofToString(f),i*factor, 40); // frequencies
-
-            
-        }
-    }
-    glPopMatrix();
-}
-
-//------------------------------------------------------------
-void ofApp::plot(vector<float>& buffer, float scale, float offset, bool freq) {
-	ofNoFill();
-    ofSetColor(255);
-	int n = buffer.size();
-    int factor = 10;
-    if (freq) {
-        ofDrawRectangle(0, 0,factor * n/16, plotHeight);
-        glPushMatrix();
-        glTranslatef(0, plotHeight / 2 + offset, 0);
-        ofBeginShape();
-        for (int i = 0; i < n/16; i++) {
-            ofVertex(i * factor, buffer[i] * scale);
-        }
-        ofEndShape();
-        glPopMatrix();
-    } else {
-        ofDrawRectangle(0, 0,n * 0.5, plotHeight);
-        glPushMatrix();
-        glTranslatef(0, plotHeight / 2 + offset, 0);
-        ofBeginShape();
-        for (int i = 0; i < n; i++) {
-            ofVertex(i*0.5, buffer[i] * scale);
-        }
-        ofEndShape();
-        glPopMatrix();
-    }
-}
-
-//------------------------------------------------------------
-void ofApp::audioReceived(float* input, int bufferSize, int nChannels) {
-    
-    if (!PAUSE) {
-    
-        if (mode == MIC) {
-            // store input in audioInput buffer
-            memcpy(&audioBuffer[0], input, sizeof(float) * bufferSize);
-            
-            float maxValue = 0;
-            for(int i = 0; i < bufferSize; i++) {
-                if(abs(audioBuffer[i]) > maxValue) {
-                    maxValue = abs(audioBuffer[i]);
-                }
-            }
-            for(int i = 0; i < bufferSize; i++) {
-                audioBuffer[i] /= maxValue;
-            }
-            
-        } else if (mode == NOISE) {
-            for (int i = 0; i < bufferSize; i++)
-                audioBuffer[i] = ofRandom(-1, 1);
-        } else if (mode == SINE) {
-            for (int i = 0; i < bufferSize; i++)
-                audioBuffer[i] = sinf(PI * i * mouseX / appWidth);
-        }
-        
-        fft->setSignal(&audioBuffer[0]);
-
-        float* curFft = fft->getAmplitude();
-        memcpy(&audioBins[0], curFft, sizeof(float) * fft->getBinSize());
-
-        float maxValue = 0;
-        for(int i = 0; i < fft->getBinSize(); i++) {
-            if(abs(audioBins[i]) > maxValue) {
-                maxValue = abs(audioBins[i]);
-            }
-        }
-        for(int i = 0; i < fft->getBinSize(); i++) {
-            audioBins[i] /= maxValue;
-        }
-        
-        
-        soundMutex.lock();
-        middleBuffer = audioBuffer;
-        middleBins = audioBins;
-        soundMutex.unlock();
-        
-    }
+void ofApp::mousePressed(int x, int y, int button) {
+    cout << ofVec2f(x,y) << endl;
 }
 
 //------------------------------------------------------------
 void ofApp::keyPressed(int key) {
-	switch (key) {
-	case 'm':
-		mode = MIC;
-		break;
-	case 'n':
-		mode = NOISE;
-		break;
-	case 's':
-		mode = SINE;
-		break;
-	}
-    
-    if (key == 'p') { // snapshot of audio
+	
+    if (key == ' ') { // snapshot of audio
         PAUSE = !PAUSE;
-        cout << "PAUSE : " + ofToString(PAUSE) << endl;
+        if (PAUSE) {
+            fft.stopSoundStream();
+            cout << "... Stopping soundStream ..." << endl;
+        } else {
+            fft.startSoundStream();
+            cout << "... Starting soundStream ..." << endl;
+        }
     }
 
     if (key == 'e') { // export midi file
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,53 +1,49 @@
 #pragma once
 
 #include "ofMain.h"
-#include "ofxFft.h"
 #include "ofxMidiFile.h"
 #include "ofxGui.h"
+#include "ofxEasyFft.h"
 
-enum {SINE, MIC, NOISE};
+
 
 class ofApp : public ofBaseApp {
 public:
 	void setup();
-    void timePeaks(vector<float> buffer, float scale, float offset, float threshold);
-    void freqPeaks(vector<float> buffer, float scale, float offset, float threshold);
-	void plot(vector<float>& buffer, float scale, float offset, bool freq);
-	void audioReceived(float* input, int bufferSize, int nChannels);
+    void update();
 	void draw();
 	void keyPressed(int key);
+    void mousePressed(int x, int y, int button);
+    
+    // fft & calculation
+    ofxEasyFft fft;
+    void plot(vector<float>& buffer, float scale, float threshold);
+    void timePeaks(vector<float>& buffer, float scale, float threshold);
+    void freqPeaks(vector<float>& buffer, float scale, float threshold);
     
     void shuffle_midiNotes();
     void shuffle_timeMarkers();
+    
     int roundUp(int numToRound, int multiple);
     
     void exportMidi();
 
-	int plotHeight, bufferSize;
-    
-    ofSoundStream soundStream;
-
-	ofxFft* fft;
-    
-    bool isActive = true;
-	
-	int mode;
-	
-	int appWidth, appHeight;
-	
-	ofMutex soundMutex;
-	vector<float> drawBins, middleBins, audioBins;
-	vector<float> drawBuffer, middleBuffer, audioBuffer;
+    // vars
+    int plotHeight;
     
     // Midi
     ofxMidiFile midiFile;
     vector<int> midiNotes;
     vector<float> timeMarkers;
     
-    bool PAUSE = false;
-    
+    bool PAUSE = false; // stop / start soundStream
     
     // Gui
     ofxPanel gui;
     ofxFloatSlider freq_threshold, time_threshold;
+    ofxButton generate;
+    
+    // Title
+    ofTrueTypeFont    verdana30;
+    string TITLE = "Audio JET";
 };
